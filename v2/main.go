5a827951a4fcadd3d18336cccd8c22e33e54f11f
Patrick Kelly <patrick.kelly@zuora.com>
2018-04-01 13:40:55 -1000

    first commit

---
+++ v2/main.go
@@ -0,0 +1,100 @@
+// v2: use a shared abort function
+// https://play.golang.org/p/LDuyBGXGeyg
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// Configuration constants for the program. Done as constants so that
+// program will run in go playground (no command line required).
+const (
+	FuelNeededToGetToStore = 5
+	FuelNeededToGetHome    = 4
+	EggsAvailableAtStore   = 24
+	CostPerEgg             = 1
+	FuelInCar              = 15
+	DollarsInWallet        = 10
+	EggsRequired           = 6
+)
+
+// Shopper is a resource to go buy eggs.
+type Shopper struct {
+	Fuel    int
+	Dollars int
+	Eggs    int
+}
+
+// String method produces a string from a Shopper.
+func (s Shopper) String() string {
+	return fmt.Sprintf("fuel=%d, dollars=%d, eggs=%d", s.Fuel, s.Dollars, s.Eggs)
+}
+
+// NewShopper constructs a new Shopper with specified fuel, dollars, but 0 eggs.
+func NewShopper(fuelInCar, dollarsInWallet int) Shopper {
+	return Shopper{
+		Fuel:    fuelInCar,
+		Dollars: dollarsInWallet,
+		Eggs:    0,
+	}
+}
+
+// Drive reduces fuel by distance. If there's not enough fuel, we'll run out of gas.
+func (s Shopper) Drive(fuelRequired int) (Shopper, error) {
+	s.Fuel -= fuelRequired
+	if s.Fuel < 0 {
+		err := fmt.Errorf("ran out of gas %d from destination", s.Fuel*-1)
+		s.Fuel = 0
+		return s, err
+	}
+
+	return s, nil
+}
+
+// BuyEggs checks if there are enough eggs, and if the shopper can afford them.
+// Add eggs, reduce dollars if possible to buy.
+func (s Shopper) BuyEggs(howMany int) (Shopper, error) {
+	if howMany > EggsAvailableAtStore {
+		return s, fmt.Errorf("there are only %d eggs available, but we need %d", EggsAvailableAtStore, howMany)
+	}
+
+	totalCost := howMany * CostPerEgg
+
+	if totalCost > s.Dollars {
+		return s, fmt.Errorf("only have %d dollars, but need %d to buy %d eggs", s.Dollars, totalCost, howMany)
+	}
+
+	s.Eggs += howMany
+	s.Dollars -= totalCost
+
+	return s, nil
+}
+
+// FatalIfErrNotNil stops the program if err is not nil.
+func FatalIfErrNotNil(err error) {
+	if err != nil {
+		log.Fatalf("could not complete shopping: %s", err)
+	}
+}
+
+func main() {
+
+	shopper := NewShopper(FuelInCar, DollarsInWallet)
+
+	log.Printf("gonna try and buy eggs")
+	log.Printf("shopper: %s", shopper)
+
+	shopper, err := shopper.Drive(FuelNeededToGetToStore)
+	FatalIfErrNotNil(err)
+
+	shopper, err = shopper.BuyEggs(EggsRequired)
+	FatalIfErrNotNil(err)
+
+	shopper, err = shopper.Drive(FuelNeededToGetHome)
+	FatalIfErrNotNil(err)
+
+	log.Printf("got the eggs!")
+	log.Printf("shopper: %s", shopper)
+}
---
+++ v6/main.go
@@ -0,0 +1,110 @@
+// v6: use a function generator, plus closure
+// https://play.golang.org/p/ZroV_GTPZng
+
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+// Configuration constants for the program. Done as constants so that
+// program will run in go playground (no command line required).
+const (
+	FuelNeededToGetToStore = 5
+	FuelNeededToGetHome    = 4
+	EggsAvailableAtStore   = 24
+	CostPerEgg             = 1
+	FuelInCar              = 15
+	DollarsInWallet        = 10
+	EggsRequired           = 6
+)
+
+// Shopper is a resource to go buy eggs.
+type Shopper struct {
+	Fuel    int
+	Dollars int
+	Eggs    int
+}
+
+// String method produces a string from a Shopper.
+func (s Shopper) String() string {
+	return fmt.Sprintf("fuel=%d, dollars=%d, eggs=%d", s.Fuel, s.Dollars, s.Eggs)
+}
+
+// NewShopper constructs a new Shopper with specified fuel, dollars, but 0 eggs.
+func NewShopper(fuelInCar, dollarsInWallet int) Shopper {
+	return Shopper{
+		Fuel:    fuelInCar,
+		Dollars: dollarsInWallet,
+		Eggs:    0,
+	}
+}
+
+// Drive reduces fuel by distance. If there's not enough fuel, we'll run out of gas.
+func Drive(s Shopper, fuelRequired int) (Shopper, error) {
+
+	s.Fuel -= fuelRequired
+	if s.Fuel < 0 {
+		err := fmt.Errorf("ran out of gas %d from destination", s.Fuel*-1)
+		s.Fuel = 0
+		return s, err
+	}
+
+	return s, nil
+}
+
+// BuyEggs checks if there are enough eggs, and if the shopper can afford them.
+// Add eggs, reduce dollars if possible to buy.
+func BuyEggs(s Shopper, howMany int) (Shopper, error) {
+
+	if howMany > EggsAvailableAtStore {
+		return s, fmt.Errorf("there are only %d eggs available, but we need %d", EggsAvailableAtStore, howMany)
+	}
+
+	totalCost := howMany * CostPerEgg
+
+	if totalCost > s.Dollars {
+		return s, fmt.Errorf("only have %d dollars, but need %d to buy %d eggs", s.Dollars, totalCost, howMany)
+	}
+
+	s.Eggs += howMany
+	s.Dollars -= totalCost
+
+	return s, nil
+}
+
+// ErrCheckFunc creates a function that check for error before executing another function.
+func ErrCheckFunc(f func(Shopper, int) (Shopper, error), arg int) func(error, Shopper) (error, Shopper) {
+
+	return func(err error, s Shopper) (error, Shopper) {
+		if err != nil {
+			return err, s
+		}
+
+		s, err = f(s, arg)
+		return err, s
+	}
+
+}
+
+func main() {
+
+	shopper := NewShopper(FuelInCar, DollarsInWallet)
+
+	log.Printf("gonna try and buy eggs")
+	log.Printf("shopper: %s", shopper)
+
+	driveToStore := ErrCheckFunc(Drive, FuelNeededToGetToStore)
+	buyEggs := ErrCheckFunc(BuyEggs, EggsRequired)
+	driveHome := ErrCheckFunc(Drive, FuelNeededToGetHome)
+
+	err, shopper := driveHome(buyEggs(driveToStore(nil, shopper)))
+
+	if err != nil {
+		log.Fatalf("could not complete shopping: %s", err)
+	}
+
+	log.Printf("got the eggs!")
+	log.Printf("shopper: %s", shopper)
+}
